5976157fe1b317e2550dcf8fddb058a35a883a8c
Corentin Le Bail <corentin@speedinfo.fr>
2018-05-04 14:20:52 +0200

    On essaie de faire mieux

---
+++ SPlateauCmp.h
@@ -0,0 +1,12 @@
+#ifndef __SPlateauCmp__
+#define __SPlateauCmp__
+
+#include "CPlateau.h"
+
+struct SPlateauCmp {
+	bool operator() (CPlateau *p1, CPlateau *p2) {
+		return *p1 == *p2;
+	}
+};
+
+#endif //__SPlateauCmp__
